e7b546076cd9e9d7ccc4df53200f07b0fea37fa4
sanjay-29218 <sanjay29218@gmail.com>
2023-01-19 09:49:41 +0545

    minor change

--- com/Navbar.jsx
+++ com/Navbar.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
-import Link from 'next/link'
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import { useRouter } from 'next/router';
-const Navbar = ({setState}) => {
-  const router = useRouter()
+import React from "react";
+import Link from "next/link";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { useRouter } from "next/router";
+const Navbar = ({ setState,handlePokemons }) => {
+  const router = useRouter();
   return (
-    <div className='p-5  sticky top-0 shadow-lg bg-slate-100 flex gap-10 items-center  '>
-     <div>
-      {router.pathname !== '/' && <Link href={'/'}><ArrowBackIcon/></Link>}
-     </div>
-            <Link href={'/'}>            <img onClick={()=>{setState(true)}}  src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" className='h-10' alt="" />
-</Link>
+    <div className="p-5  sticky top-0 shadow-lg bg-slate-100 flex gap-10 items-center  ">
+      <div>
+        {router.pathname !== "/" && (
+          <Link href={"/"}>
+            <ArrowBackIcon />
+          </Link>
+        )}
+      </div>
+      <Link href={"/"}>
+        {" "}
+        <img
+          onClick={() => {
+            setState(true);
+            handlePokemons();
+          }}
+          src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
+          className="h-10"
+          alt=""
+        />
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
