80f1c79a2a29f0a5920dfd2ebc40c62148855603
sanjay-29218 <sanjay29218@gmail.com>
2023-01-19 08:58:03 +0545

    minor changees

--- com/Navbar.jsx
+++ com/Navbar.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Link from 'next/link'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { useRouter } from 'next/router';
-const Navbar = () => {
+const Navbar = ({setState}) => {
   const router = useRouter()
   return (
     <div className='p-5  sticky top-0 shadow-lg bg-slate-100 flex gap-10 items-center  '>
      <div>
       {router.pathname !== '/' && <Link href={'/'}><ArrowBackIcon/></Link>}
      </div>
-            <Link href={'/'}>            <img  src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" className='h-10' alt="" />
+            <Link href={'/'}>            <img onClick={()=>{setState(true)}}  src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" className='h-10' alt="" />
 </Link>
     </div>
   )
